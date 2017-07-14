5385b6bef85323702100e72c9e11d584573f4651
欣曄 <linvic@garena.com>
2017-07-14 12:12:10 +0800

    add heroku settings

--- LINE_BOT_phptopython/settings.py
+++ LINE_BOT_phptopython/settings.py
@@ -80,3 +80,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import os
+if os.getenv('DATABASE_URL') is not None:
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
+
